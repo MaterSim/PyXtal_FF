4d4ee6b00807036144c7c52976e232ecb78d212e
yanxon <hg.yanxon@gmail.com>
2019-01-22 20:00:30 -0800

    run SNAP

---
+++ SNAP/example/fitsnap.py
@@ -0,0 +1,10 @@
+from ase.io import read, write
+from ase import Atoms, Atom
+from ase.calculators.lammpsrun import LAMMPS
+
+data = read('../../POSCARs/POSCAR-NaCl')
+
+calc = LAMMPS()
+data.set_calculator(calc)
+
+print(data.write_lammps_in())
