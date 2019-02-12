77404ce0a4d7daf72895e15a8fa00ac88f6fa0be
Howard Yanxon <hg.yanxon@gmail.com>
2019-02-11 21:17:14 -0800

    change to class type;

--- test/cu_bispectrum.py
+++ test/cu_bispectrum.py
@@ -13,30 +13,31 @@ from utilities.assembler import assembler
 import pandas as pd
 
 
-# Read json file and convert them to pymatgen structure object 
-# and the corresponding energies and volumes.
-
-files = ["AIMD.json"] #"Elastic.json", "Surface.json", "Vacancy.json"]
+files = ["AIMD.json", "Elastic.json", "Surface.json", "Vacancy.json"]
 profile = dict(Cu=dict(r=1.0, w=1.0))
-Rc = 3.7
+Rc = 4.5
 twojmax = 6
 diagonal = 3
 force = True
+save = False
+
 
 class Cu_bispectrum(object):
     """
     
     """
-    def __init__(self, files, profile, Rc, twojmax, diagonal, force):
+    def __init__(self, files, profile, Rc, twojmax, diagonal, force, save=False):
         self.files = files
         self.profile = profile
         self.Rc = Rc
         self.twojmax = twojmax
         self.diagonal = diagonal
         self.force = force
+        self.save = save
         
         self.structures, self.y = self.get_structures_energies()
-        self.X = self.convert_to_bispectrum()
+        self.X = self.convert_to_bispectrum(self.save)
+        self.linear_regression()
         
         
     def get_structures_energies(self):
@@ -50,7 +51,7 @@ class Cu_bispectrum(object):
             with open("../datasets/Cu/training/"+file) as f:
                 data = json.load(f)
         
-            for struc in data[:5]:
+            for struc in data:
                 lat = struc['structure']['lattice']['matrix']
                 species = []
                 positions = []
@@ -78,7 +79,7 @@ class Cu_bispectrum(object):
         return structures, y
     
     
-    def convert_to_bispectrum(self):
+    def convert_to_bispectrum(self, save):
         time0 = time.time()
         
         sna = []        
@@ -98,38 +99,30 @@ class Cu_bispectrum(object):
         print(f"This is the time it takes to generate bispectrum coefficients \
               for {len(self.structures)} structures: {t} s")
 
+        if save:            
+            np.savetxt("bispectrum.csv", sna, delimiter=",")
+            np.savetxt("output.csv", self.y, delimiter=",")
+
+        
         return sna
 
 
-# Apply linear regression and evaluate
+    def linear_regression(self):
+        time0 = time.time()
+        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size=0.4, random_state=13)
+        reg = LinearRegression().fit(X_train, y_train)
+        predict_train = reg.predict(X_train)
+        predict_test = reg.predict(X_test)
+        
+        r2_train = reg.score(X_train, y_train)
+        mae_train = mean_absolute_error(y_train, predict_train)/108*1000
+        r2_test = reg.score(X_test, y_test)
+        mae_test = mean_absolute_error(y_test, predict_test)/108*1000
 
+        d = {'train_r2': [r2_train],'train_mae': [mae_train], 'test_r2': [r2_test], 'test_mae': [mae_test]}
+        df = pd.DataFrame(d)
+        print(df)
 
-
-#X_train, X_test, y_train, y_test = train_test_split(sna, energies, test_size=0.4, random_state=13)
-#
-#lr_time0 = time.time()
-#reg = LinearRegression().fit(X_train, y_train)
-#predict_train = reg.predict(X_train)
-#predict_test = reg.predict(X_test)
-#lr_time1 = time.time()
-#lr_time = round(lr_time1 - lr_time0, 2)
-#print(f"This is the time it takes running the linear regression: {lr_time} s")
-#
-#r2_train = reg.score(X_train, y_train)
-#mae_train = mean_absolute_error(y_train, predict_train)
-#r2_test = reg.score(X_test, y_test)
-#mae_test = mean_absolute_error(y_test, predict_test)
-
-
-# Print
-
-#d = {'train_r2': [r2_train], 
-#     'train_mae': [mae_train],
-#     'test_r2': [r2_test],
-#     'test_mae': [mae_test]}
-#
-#df = pd.DataFrame(d)
-#print(df)
-
+        
 if __name__ == '__main__':
     Cu_bispectrum(files, profile, Rc, twojmax, diagonal, force)
