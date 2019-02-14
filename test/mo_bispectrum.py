080c3ad47f0d51e0d41c3fb2b948cefd47d76e54
Howard Yanxon <32254481+yanxon@users.noreply.github.com>
2019-02-13 21:23:29 -0800

    add Mo

--- test/mo_bispectrum.py
+++ test/mo_bispectrum.py
@@ -12,13 +12,14 @@ from descriptors.snap import bispectrum
 from utilities.assembler import assembler
 import pandas as pd
 
-
-files = ["AIMD_NPT.json"] #["AIMD.json", "Elastic.json", "Surface.json", "Vacancy.json"]
+directory = "../datasets/Mo/training/"
+files = ["AIMD_NPT.json"] #"AIMD_NVT.json", "Elastic.json", "GB.json", "Surface.json", "Vacancy.json"]
 profile = dict(Mo=dict(r=1.0, w=1.0))
 Rc = 4.615858
 twojmax = 6
 diagonal = 3
 force = True
+stress = True
 save = False
 
 
@@ -26,13 +27,15 @@ class Cu_bispectrum(object):
     """
     
     """
-    def __init__(self, files, profile, Rc, twojmax, diagonal, force, save=False):
+    def __init__(self, files, profile, Rc, twojmax, diagonal, 
+                 force=False, stress=False, save=False):
         self.files = files
         self.profile = profile
         self.Rc = Rc
         self.twojmax = twojmax
         self.diagonal = diagonal
         self.force = force
+        self.stress = stress
         self.save = save
         
         self.structures, self.y = self.get_structures_energies()
@@ -49,10 +52,10 @@ class Cu_bispectrum(object):
 
         
         for file in files:
-            with open("../datasets/Mo/training/"+file) as f:
+            with open(directory+file) as f:
                 data = json.load(f)
         
-            for struc in data:
+            for struc in data[:5]:
                 lat = struc['structure']['lattice']['matrix']
                 species = []
                 positions = []
@@ -62,117 +65,141 @@ class Cu_bispectrum(object):
                 structure = Structure(lat, species, positions)
                 structures.append(structure)
                 
-                if self.force == False:
-                    y.append(struc['data']['energy_per_atom'])
-                    n_atoms.append(structure.num_sites)
-                else:
-                    y.append(struc['data']['energy_per_atom'])
-                    n_atoms.append(structure.num_sites)
+                # append energies in y
+                y.append(struc['data']['energy_per_atom'])
+                n_atoms.append(structure.num_sites)
+                
+                # append force in y
+                if self.force == True:                    
                     fs = np.ravel(struc['data']['forces'])
                     for f in fs:
                         y.append(f)
                         n_atoms.append(1.)
+                    # append stress in y
+                    if self.stress == True:
+                        ss = np.ravel(struc['data']['virial_stress'])
+                        for s in ss:
+                            y.append(s)
+                            n_atoms.append(1.0)
 
                 self.volumes.append(structure.volume)
-        
+
         time1 = time.time()
         t = round(time1 - time0, 2)
-        print(f"This is the time it takes to convert json files to \
-              structures: {t} s")
-        
+        print(f"The time it takes to convert json files to structures: {t} s")
+
         return structures, np.vstack((y, n_atoms))
     
     
     def convert_to_bispectrum(self, save):
         time0 = time.time()
         
-        sna = []        
+        snap = []        
         for i in range(len(self.structures)):
-            bispectrum(self.structures[i], self.Rc, self.twojmax, self.profile, 
+            bispectrum(self.structures[i], self.Rc, self.twojmax, self.profile,
                        diagonal=self.diagonal)
             bispec = assembler(atom_type=['Cu'], volume=self.volumes[i], 
-                               force=self.force, stress=False)
-            if sna == []:
-                sna = bispec.bispectrum_coefficients
+                               force=self.force, stress=self.stress)
+            if snap == []:
+                snap = bispec.bispectrum_coefficients
             else:
-                sna = np.vstack((sna, bispec.bispectrum_coefficients))
+                snap = np.vstack((snap, bispec.bispectrum_coefficients))
         
         
         time1 = time.time()
         t = round(time1 - time0, 2)
-        print(f"This is the time it takes to generate bispectrum coefficients \
+        print(f"The time it takes to generate bispectrum coefficients \
               for {len(self.structures)} structures: {t} s")
-
-        if save:            
-            np.savetxt("bispectrum.csv", sna, delimiter=",")
-            np.savetxt("output.csv", self.y, delimiter=",")
-
         
-        return sna
+        return snap
 
 
     def linear_regression(self):
+        """
+        perform linear regression
+        """
         time0 = time.time()
         ts = 0.4
         rs = 107
-        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y[0], test_size=ts, random_state=rs)
-        X_train1, X_test1, y_train1, y_test1 = train_test_split(self.X, self.y[1], test_size=ts, random_state=rs)
+        X_train, X_test, y_train, y_test = train_test_split(self.X, 
+                                                            self.y[0], 
+                                                            test_size=ts, 
+                                                            random_state=rs)
+        # To obtain # of atoms in a unit cell
+        _, _, n_atoms_train, n_atoms_test = train_test_split(self.X, 
+                                                             self.y[1], 
+                                                             test_size=ts, 
+                                                             random_state=rs)
 
         reg = LinearRegression().fit(X_train, y_train)
         
-        # Training evalution
-        X_forces_train, X_energies_train = [], []
-        y_forces_train, y_energies_train = [], []
-        n_atoms_train = []
-        for i, natom in enumerate(y_train1):
-            if natom == 1.:
-                X_forces_train.append(X_train[i])
-                y_forces_train.append(y_train[i])
-            else:
-                X_energies_train.append(X_train[i])
-                y_energies_train.append(y_train[i])
-                n_atoms_train.append(natom)
-        print(n_atoms_train)
-        yp_forces_train = reg.predict(X_forces_train)
-        mae_forces_train = mean_absolute_error(y_forces_train, yp_forces_train)
-        r2_forces_train = reg.score(X_forces_train, y_forces_train)
-        yp_energies_train = reg.predict(X_energies_train)
-        mae_energies_train = mean_absolute_error(y_energies_train, yp_energies_train)
-        r2_energies_train = reg.score(X_energies_train, y_energies_train)
+        # Evaluate training dataset
+        mae_E_train, r2_E_train, mae_F_train, r2_F_train = \
+            self.evaluate_mae_rsquare(reg, n_atoms_train, X_train, y_train)
+            
+        # Evaluate test dataset
+        mae_E_test, r2_E_test, mae_F_test, r2_F_test = \
+            self.evaluate_mae_rsquare(reg, n_atoms_test, X_test, y_test)
+            
+        # Print train
+        print(f"Score for training dataset")
+        d_train = {'energy_r2': [r2_E_train], 
+                   'energy_mae': [mae_E_train], 
+                   'force_r2': [r2_F_train], 
+                   'force_mae': [mae_F_train]}
+        df_train = pd.DataFrame(d_train)
+        print(df_train)
         
-        print(mae_energies_train, r2_energies_train, mae_forces_train, r2_forces_train)
-
-        # Training evalution
-        X_forces_test, X_energies_test = [], []
-        y_forces_test, y_energies_test = [], []
-        n_atoms_test = []
-        for i, natom in enumerate(y_test1):
-            if natom == 1.:
-                X_forces_test.append(X_test[i])
-                y_forces_test.append(y_test[i])
+        # Print test
+        print(f"Score for test dataset")
+        d_test = {'energy_r2': [r2_E_test], 
+                   'energy_mae': [mae_E_test], 
+                   'force_r2': [r2_F_test], 
+                   'force_mae': [mae_F_test]}
+        df_test = pd.DataFrame(d_test)
+        print(df_test)
+        
+        time1 = time.time()
+        t = round(time1 - time0, 2)
+        print(f"The time it takes to perform linear regression: {t} s")
+        
+        
+    def evaluate_mae_rsquare(self, regression, n_atoms, X, y):
+        """
+        Evaluate the train or test dataset.
+        
+        Returns
+        -------
+             mean absolute error and r2 for energies and force.
+        """
+        X_forces, X_energies = [], []
+        y_forces, y_energies = [], []
+        natoms = []
+        for i, atom in enumerate(n_atoms):
+            if atom == 1.:
+                X_forces.append(X[i])
+                y_forces.append(y[i])
             else:
-                X_energies_test.append(X_test[i])
-                y_energies_test.append(y_test[i])
-                n_atoms_test.append(natom)
-        yp_forces_test = reg.predict(X_forces_test)
-        mae_forces_test = mean_absolute_error(y_forces_test, yp_forces_test)
-        r2_forces_test = reg.score(X_forces_test, y_forces_test)
-        yp_energies_test = reg.predict(X_energies_test)
-        mae_energies_test = mean_absolute_error(np.asarray(y_energies_test), np.asarray(yp_energies_test))
-        r2_energies_test = reg.score(X_energies_test, y_energies_test)
-
-        print(mae_energies_test, r2_energies_test, mae_forces_test, r2_forces_test)
-
-
-        #y_pred = reg.predict(X_test)
-        #mae = mean_absolute_error(y_pred, y_test)
-        #r2 = reg.score(X_test, y_test)
-        #print(mae, r2)
-
-        #d = {'train_r2': [r2_train],'train_mae': [mae_train], 'test_r2': [r2_test], 'test_mae': [mae_test]}
-        #df = pd.DataFrame(d)
-        #print(df)
+                X_energies.append(X[i])
+                y_energies.append(y[i])
+                natoms.append(atom)
+                
+        # Evaluate energy
+        yp_energies = regression.predict(X_energies)
+        mae_energies = mean_absolute_error(y_energies, yp_energies)
+        r2_energies = regression.score(X_energies, y_energies)
+        
+        # Evaluate force
+        yp_forces = regression.predict(X_forces)
+        mae_forces = mean_absolute_error(y_forces, yp_forces)
+        r2_forces = regression.score(X_forces, y_forces)
+        
+        return mae_energies, r2_energies, mae_forces, r2_forces        
 
         
 if __name__ == '__main__':
-    Cu_bispectrum(files, profile, Rc, twojmax, diagonal, force)
+    Cu_bispectrum(files, profile, Rc, twojmax, diagonal, force, stress)
+    
+#d = {'train_r2': [r2_train],'train_mae': [mae_train], 'test_r2': [r2_test], 'test_mae': [mae_test]}
+#df = pd.DataFrame(d)
+#print(df)
