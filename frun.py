4b6336d66fe8414c4f94eb8281b43d428e095b6f
Howard Yanxon <32254481+yanxon@users.noreply.github.com>
2019-07-21 20:16:42 -0700

    Create frun.py

---
+++ frun.py
@@ -0,0 +1,25 @@
+from monty.serialization import loadfn
+import json
+from mapp.descriptors.fbehlerparrinello import BehlerParrinello
+from mapp.models.fneuralnetwork import fNeuralNetwork
+
+data = loadfn("mapp/datasets/Si/training.json")[:2]
+
+descriptors = loadfn("BehlerParrinello.json")
+
+features = {}
+#structures = [d['structure'] for d in data]
+structures = []
+for i, d in enumerate(data):
+    structures.append(d['structure'])
+    features[i] = {}
+    features[i]['energy'] = d['outputs']['energy']/len(d['structure'])
+    features[i]['forces'] = d['outputs']['forces']
+
+symmetry = {'G2': {'eta': [0.036, 0.071,]},
+            'G4': {'lambda': [-1, 1], 'zeta':[1], 'eta': [0.036, 0.071,]}}
+
+#descriptors = BehlerParrinello(symmetry, Rc=5.2, derivative=True)
+
+model = fNeuralNetwork(elements=['Si'])
+model.train(None, descriptors=descriptors, features=features, save=False)
