079852dadac3251ccd5ead869c2a0d2b629fa998
Qiang Zhu <alecfans@gmail.com>
2020-03-29 09:40:39 -0700

    add calculator

---
+++ pyxtal_ff/calculator/__init__.py
@@ -0,0 +1,106 @@
+from pyxtal_ff import PyXtal_FF
+import numpy as np
+np.set_printoptions(formatter={'float': '{: 8.4f}'.format})
+from ase import units
+from pyxtal_ff.utilities import compute_descriptor, compress_descriptors
+from ase.calculators.calculator import Calculator, all_changes#, PropertyNotImplementedError
+from ase.optimize import BFGS
+from pyxtal_ff.calculator.mushybox import mushybox
+from pyxtal_ff.calculator.elastic import get_elementary_deformations, get_elastic_tensor
+
+class PyXtalFFCalculator(Calculator):
+    implemented_properties = ['energy', 'forces', 'stress']
+    nolabel = True
+
+    def __init__(self, **kwargs):
+        Calculator.__init__(self, **kwargs)
+
+    def calculate(self, atoms=None,
+                  properties=['energy'],
+                  system_changes=all_changes):
+
+        Calculator.calculate(self, atoms, properties, system_changes)
+
+        chem_symbols = list(set(atoms.get_chemical_symbols()))
+        self.ff = PyXtal_FF(model={'system': chem_symbols}, 
+                            logo=self.parameters.logo)
+        self.ff.run(mode='predict', mliap=self.parameters.mliap)
+
+        desp = compute_descriptor(self.ff._descriptors, atoms)
+
+        energy, forces, stress = self.ff.model.calculate_properties(desp, bforce=True, bstress=True)
+
+        self.results['energy'] = energy
+        self.results['free_energy'] = energy
+        self.results['forces'] = forces
+        self.results['stress']  = (stress/atoms.get_volume())[[0, 1, 2, 5, 4, 3]]
+
+def elastic_tensor(atoms, calc):
+    # Compute elastic constants
+    # print("\n--------Calculating the Elastic constants")
+    # Create elementary deformations
+    systems = get_elementary_deformations(atoms, d=1)
+    
+    # Run the stress calculations on deformed cells
+    for i, system in enumerate(systems):
+        system.set_calculator(calc)
+        dyn = BFGS(system)
+        dyn.run(fmax=0.01)
+        #print(system.get_potential_energy(), system.get_stress()/units.GPa)
+    # Elastic tensor by internal routine
+    Cijs, Bij, names, matrix = get_elastic_tensor(atoms, systems)
+    return Cijs, names, matrix
+
+def elastic_properties(C):
+    Kv = C[:3,:3].mean()
+    Gv = (C[0,0]+C[1,1]+C[2,2] - (C[0,1]+C[1,2]+C[2,0]) + 3*(C[3,3]+C[4,4]+C[5,5]))/15
+    Ev = 1/((1/(3*Gv))+(1/(9*Kv)))
+    vv  = 0.5*(1-((3*Gv)/(3*Kv+Gv))); 
+
+    S = np.linalg.inv(C)
+    Kr = 1/((S[0,0]+S[1,1]+S[2,2])+2*(S[0,1]+S[1,2]+S[2,0])) 
+    Gr = 15/(4*(S[0,0]+S[1,1]+S[2,2])-4*(S[0,1]+S[1,2]+S[2,0])+3*(S[3,3]+S[4,4]+S[5,5])) 
+    Er = 1/((1/(3*Gr))+(1/(9*Kr))) 
+    vr = 0.5*(1-((3*Gr)/(3*Kr+Gr))) 
+
+    Kh = (Kv+Kr)/2    
+    Gh = (Gv+Gr)/2    
+    Eh = (Ev+Er)/2    
+    vh = (vv+vr)/2   
+    return Kv, Gv, Ev, vv, Kr, Gr, Er, vr, Kh, Gh, Eh, vh
+
+
+def optimize(atoms, box=False, fmax=0.01):
+    if box:
+        box = mushybox(atoms)
+        dyn = BFGS(box)
+    else:
+        dyn = BFGS(atoms)
+
+    dyn.run(fmax=fmax)
+    return atoms
+
+if  __name__ == "__main__":
+
+    from optparse import OptionParser
+    from ase.build import bulk
+
+    parser = OptionParser()
+    parser.add_option("-f", "--file", dest="file",
+                      help="pretrained file from pyxtal_ff, REQUIRED",
+                      metavar="file")
+
+    (options, args) = parser.parse_args()
+    print(options.file)
+    calc = PyXtalFFCalculator(mliap=options.file, logo=False)
+    si = bulk('Si', 'diamond', a=5.459, cubic=True)
+    si.set_calculator(calc)
+    print(si.get_potential_energy())
+    print(si.get_forces())
+    print(si.get_stress())
+    Cijs, names, C = elastic_tensor(si, calc)
+    for name, Cij in zip(names, Cijs):
+        print("{:s}: {:8.2f}(GPa)".format(name, Cij))
+
+    print(elastic_properties(C))
+
---
+++ pyxtal_ff/calculator/phonon.py
@@ -0,0 +1,124 @@
+import numpy as np
+from ase import Atoms
+from phonopy.api_phonopy import Phonopy
+
+class Phonon(object):
+    """
+    This module is to compute phonon from the trained Pyxtal_FF model.
+    We use phonopy API to construct and handle force constant matrix
+
+    Args:
+    atoms: ase atoms object
+    calculator: ase calculator object
+    supercell_matrix:  3x3 matrix supercell
+    primitive cell:  3x3 matrix primitive cell
+    displacement_distance: displacement distance in Angstroms
+    show_progress: Set true to display progress of calculation
+    """
+
+    def __init__(self,
+                 atoms,
+                 calculator,
+                 supercell_matrix=np.identity(3),
+                 primitive_matrix='auto',
+                 displacement_distance=0.01,
+                 show_progress=True,
+                 symmetrize=True):
+
+        self._structure = atoms
+        self._calculator = calculator
+        self._supercell_matrix = supercell_matrix
+        self._primitive_matrix = primitive_matrix
+        self._d = displacement_distance
+        self._show_progress = show_progress
+        self._symmetrize = symmetrize
+
+        self._force_constants = None
+        self._data_set = None
+
+    def get_force_constants(self, include_data_set=False):
+        """
+        calculate the force constants with phonopy 
+
+        Return: 
+        ForceConstants type object containing force constants
+        """
+
+        self.phonon = Phonopy(self._structure,
+                              self._supercell_matrix,
+                              primitive_matrix = self._primitive_matrix,
+                              is_symmetry = self._symmetrize)
+        self.phonon.get_displacement_dataset()
+        self.phonon.generate_displacements(distance=self._d) 
+        cells_with_disp = self.phonon.get_supercells_with_displacements()
+        data_set = self.phonon.get_displacement_dataset()
+        # Compute forces for the displaced structures
+        for i, cell in enumerate(cells_with_disp):
+            if self._show_progress:
+                print('displacement {} / {}'.format(i+1, len(cells_with_disp)))
+            forces = self.get_forces(cell)
+            data_set['first_atoms'][i]['forces'] = forces
+
+        self.phonon.set_displacement_dataset(data_set)
+        self.phonon.produce_force_constants()
+        self._force_constants = self.phonon.get_force_constants()
+        self._data_set = data_set
+
+        return self._force_constants
+
+    def get_forces(self, cell_with_disp):
+        """
+        Calculate the forces of a supercell using lammps
+        Args:
+        cell_with_disp: supercell from which determine the forces
+
+        Return:
+        forces: [Natoms_of_supercell, 3] array
+        """
+
+        numbers = self._structure.numbers
+        s_numbers = []
+        mult = np.linalg.det(self._supercell_matrix)
+        atom_id = 0
+        disps = cell_with_disp.get_positions()
+        for id, disp in enumerate(disps):
+            s_numbers.append(numbers[atom_id])
+            if id%mult == (mult-1):
+                atom_id += 1
+        
+        cell = self._structure.cell.dot(self._supercell_matrix)
+        supercell = Atoms(s_numbers, positions=disps, cell=cell, pbc=[1,1,1])
+        supercell.set_calculator(self._calculator)
+        return supercell.get_forces()
+
+if  __name__ == "__main__":
+
+    from optparse import OptionParser
+    from ase.build import bulk
+    from pyxtal_ff.calculator import PyXtalFFCalculator
+    from phonopy.phonon.band_structure import get_band_qpoints_and_path_connections
+
+    parser = OptionParser()
+    parser.add_option("-f", "--file", dest="file",
+                      help="pretrained file from pyxtal_ff, REQUIRED",
+                      metavar="file")
+
+    (options, args) = parser.parse_args()
+    calc = PyXtalFFCalculator(mliap=options.file, logo=False)
+    si = bulk('Si', 'diamond', a=5.459, cubic=True)
+    supercell_matrix=np.diag([2,2,2])
+    ph = Phonon(si, calc, supercell_matrix=supercell_matrix) 
+    force_constants = ph.get_force_constants()
+    ph.phonon.set_force_constants(force_constants)
+
+    #ph.phonon.auto_band_structure()
+    path = [[[0, 0, 0], [0.5, 0, 0.5], [0.375, 0.375, 0.75], [0, 0, 0], [0.5, 0.5, 0.5]]]
+    labels = ["$\\Gamma$", "X", "K", "$\\Gamma$", "L"]
+    qpoints, connections = get_band_qpoints_and_path_connections(path, npoints=51)
+    ph.phonon.run_band_structure(qpoints, path_connections=connections, labels=labels)
+
+    ph.phonon.set_mesh([10,10,10])
+    ph.phonon.set_total_DOS()
+    ph.phonon.plot_band_structure_and_dos().savefig('Si_phonon.png', dpi=300)
+    print('The detected space group is {:s} with a tolerance of {:.2e}'.format(
+          ph.phonon._symmetry.dataset['international'], ph.phonon._symprec))
