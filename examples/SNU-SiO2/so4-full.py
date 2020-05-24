d9fd3286db6603191cb6ffbb2b8e485adcb9f75e
qzhu2017 <alecfans@gmail.com>
2020-05-24 16:24:36 -0700

    update example for SiO2

--- examples/SNU-SiO2/so4-full.py
+++ examples/SNU-SiO2/so4-full.py
@@ -3,25 +3,30 @@
 #Here we use only 1000 samples
 
 from pyxtal_ff import PyXtal_FF
+import os
 
 train_data = 'OUTCAR_comp'
+if not os.path.exists(train_data):
+    print('downloading the training data')
+    os.system('wget https://raw.githubusercontent.com/MDIL-SNU/SIMPLE-NN/master/examples/SiO2/ab_initio_output/OUTCAR_comp')
 
 descriptor = {'Rc': 4.9, 
               'parameters': {'lmax': 3},
-              'N_train': 1000,
-              'ncpu': 8,
+              'N_train': 2000,
+              'ncpu': 16,
              }
  
 model = {'system' : ['Si', 'O'],
          'hiddenlayers': [30, 30],
          'activation': ['Tanh', 'Tanh', 'Linear'],
          'force_coefficient': 0.1,
-         'epoch': 500,
+         'epoch': 10,
+         #'device': 'cuda',
          'path': 'SiO2-full-batch/',
-         #'restart': 'SiO2-full-batch/30-30-checkpoint.pth',
+         'restart': 'SiO2-full-batch/30-30-checkpoint.pth',
          'optimizer': {'method': 'lbfgs'},
          }
 
-#-------------------------------- Run NN calculation ------------------------------
+#------------------------- Run NN calculation ------------------------------
 ff = PyXtal_FF(descriptors=descriptor, model=model)
 ff.run(mode='train', TrainData=train_data)
--- examples/SNU-SiO2/so4-mini.py
+++ examples/SNU-SiO2/so4-mini.py
@@ -3,13 +3,16 @@
 #Here we use 200-500 samples for each batch
 
 from pyxtal_ff import PyXtal_FF
+import os
 
 train_data = 'OUTCAR_comp'
+if not os.path.exists(train_data):
+    print('downloading the training data')
+    os.system('wget https://raw.githubusercontent.com/MDIL-SNU/SIMPLE-NN/master/examples/SiO2/ab_initio_output/OUTCAR_comp')
 
 descriptor = {'Rc': 4.9, 
               'parameters': {'lmax': 3},
-              'N_train': 100,
-              'ncpu': 8,
+              'ncpu': 16,
              }
  
 model = {'system' : ['Si', 'O'],
@@ -18,12 +21,13 @@ model = {'system' : ['Si', 'O'],
          'force_coefficient': 0.1,
          'epoch': 500,
          'batch_size': 200,
+         #'device': 'cuda',
          'path': 'SiO2-mini-batch/'
          #'restart': 'SiO2-mini-batch/30-30-checkpoint.pth',
          'memory': 'out',
          'optimizer': {'method': 'Adam'}
          }
 
-#-------------------------------- Run NN calculation ------------------------------
+#------------------------- Run NN calculation ------------------------------
 ff = PyXtal_FF(descriptors=descriptor, model=model)
 ff.run(mode='train', TrainData=train_data)
