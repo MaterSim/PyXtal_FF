1db23f53b0aeac16c6fa834f6af34b3bae096fed
qzhu2017 <alecfans@gmail.com>
2021-02-06 09:51:22 -0800

    try to fix workflow

--- pyxtal_ff/test_all.py
+++ pyxtal_ff/test_all.py
@@ -14,14 +14,15 @@ np.set_printoptions(formatter={'float': '{: 12.4f}'.format})
 def get_rotated_struc(struc, angle=0, axis='x'):
     s_new = struc.copy()
     s_new.rotate(angle, axis)
-    p_struc = Atoms(s_new.symbols.numbers, positions=s_new.positions, cell=s_new.cell, pbc=True)
+    print("rotation", s_new.positions)
+    p_struc = Atoms(s_new.symbols.numbers, positions=s_new.positions, cell=struc.cell, pbc=True)
     return p_struc
 
 def get_perturbed_struc(struc, eps):
     s_new = struc.copy()
     pos = s_new.positions
     pos[0,0] += eps
-    p_struc = Atoms(s_new.symbols.numbers, positions=pos, cell=s_new.cell, pbc=True)
+    p_struc = Atoms(s_new.symbols.numbers, positions=pos, cell=struc.cell, pbc=True)
     return p_struc
 
 surfaces = [(1, 0, 0), (1, 1, 0), (1, 1, 1)]
