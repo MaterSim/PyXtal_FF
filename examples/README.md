76e2f66ca29b94ca9baa52bd961ebbb0b6e70163
qzhu2017 <alecfans@gmail.com>
2021-03-10 23:09:07 -0800

    reorganize the example folder

--- examples/README.md
+++ examples/README.md
@@ -2,15 +2,18 @@
 
 Here are the examples of force field training applied to different dataset collected from the past literature.
 
-### UCSD-Si-214-25
+### Si-Python
 - [URL](https://github.com/materialsvirtuallab/mlearn/tree/master/data/Si)
 - [Reference](https://arxiv.org/pdf/1906.08888.pdf)
 - Description: crystalline silicon dataset from MD simulation and manual construction.
 
-### SNU-SiO2
+### SiO2-Python
 - [URL](https://github.com/MDIL-SNU/SIMPLE-NN/tree/master/examples/SiO2/ab_initio_output)
 - [Reference](https://doi.org/10.1016/j.cpc.2019.04.014)
 - Description: crystalline SiO2 dataset from NVT MD simulation.
 
-### Si-SNAP-NN-LAMMPS
+### Si-LAMMPS
+- Description: an example to explain how to run MD simulation based on the LAMMPS/MLIAP interface
+
+### SiO2-LAMMPS
 - Description: an example to explain how to run MD simulation based on the LAMMPS/MLIAP interface
--- examples/SiO2-LAMMPS/validate.py
+++ examples/SiO2-LAMMPS/validate.py
@@ -10,7 +10,6 @@ import warnings
 warnings.simplefilter("ignore")
 
 des, folder = "sna", "SiO2-snap"
-#des, folder = "so3", "Si-so3"
 mliap  = folder + "/30-30-checkpoint.pth"
 lmpiap = folder + "/NN_weights.txt"
 lmpdes = folder + "/DescriptorParam.txt"
@@ -18,7 +17,9 @@ lmpdes = folder + "/DescriptorParam.txt"
 
 # initial silicon crystal
 si = read('lt_quartz.cif')
-si.positions[0,0] += (random() - 0.5)
+# set the ordering
+si.set_tags([2]*3+[1]*6) 
+#si.positions[0,0] += (random() - 0.5)
 
 # ase pyxtal_ff calculator
 ff = PyXtal_FF(model={'system': ["Si", "O"]}, logo=False)
@@ -35,7 +36,7 @@ lmp = lammps(lammps_name, cmd_args, comm)
 
 parameters = ["mass * 1.0",
               "pair_style mliap model nn " + lmpiap + " descriptor " + des + " " + lmpdes,
-              "pair_coeff * * Si O"
+              "pair_coeff * * O Si"
               ]
 
 calc_lmp = LAMMPSlib(lmp=lmp, lmpcmds=parameters)
