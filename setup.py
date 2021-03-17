3c7961dcc0cfc522fb300c721e18074210687c5e
qzhu2017 <alecfans@gmail.com>
2021-03-16 20:40:55 -0700

    tmp fix for phonopy

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         'matplotlib>=2.0.0',
         'ase>=3.20.1',
         'torch>=1.1.0',
-        'phonopy>=2.3.2',
+        'phonopy==2.3.2', #tmp fix
         'spglib>=1.12.1',
         'monty>=3.0.2',
         'seekpath>=1.9.5',
