f1fed580a64c9e15a4962dbe1315d6275e1c52ae
qzhu2017 <alecfans@gmail.com>
2021-05-21 10:48:12 -0700

    Finalized SO3 code #39

--- examples/SiO2-LAMMPS/train.py
+++ examples/SiO2-LAMMPS/train.py
@@ -2,22 +2,34 @@ from pyxtal_ff import PyXtal_FF
 
 trainData = "data/SiO2.db"
 
-descriptor = {'Rc': 4.9, 
-              'type': 'SNAP',
-              'weights': {'Si': 14.0, 'O': 8.0},
-              'parameters': {'lmax': 3},
-              'base_potential': {'inner': 1.0, 'outer': 2.0}, 
-              'ncpu': 16,
-              'N_train': 250,
-             }
+if False: #True:
+    folder = 'SiO2-snap/'
+    descriptor = {'Rc': 4.9, 
+                  'type': 'SNAP',
+                  'weights': {'Si': 14.0, 'O': 8.0},
+                  'parameters': {'lmax': 3},
+                  'base_potential': {'inner': 1.0, 'outer': 2.0}, 
+                  'ncpu': 16,
+                  'N_train': 250,
+                 }
+else:
+    folder = 'SiO2-so3/'
+    descriptor = {'Rc': 4.9, 
+                  'type': 'SO3',
+                  'weights': {'Si': 0.5, 'O': 0.5},
+                  'parameters': {'lmax': 4, 'nmax': 3},
+                  'base_potential': {'inner': 1.0, 'outer': 2.0}, 
+                  'ncpu': 16,
+                  'N_train': 250,
+                 }
+
  
 model = {'system' : ['Si', 'O'],
          'hiddenlayers': [30, 30],
          'force_coefficient': 0.1,
          'epoch': 100,
-         #'batch_size': 1000,
-         'path': 'SiO2-snap/',
-         'restart': 'SiO2-snap/30-30-checkpoint.pth',
+         'path': folder,
+         #'restart': folder + '/30-30-checkpoint.pth',
          'alpha': 1e-6,
          }
 
--- examples/SiO2-LAMMPS/validate.py
+++ examples/SiO2-LAMMPS/validate.py
@@ -1,4 +1,3 @@
-from random import random
 import numpy as np
 from ase.io import read
 from ase import units
@@ -7,69 +6,71 @@ from lammps import lammps
 from pyxtal_ff import PyXtal_FF
 from pyxtal_ff.calculator import PyXtalFFCalculator
 from pyxtal_ff.calculator.lammpslib import LAMMPSlib
+import warnings; warnings.simplefilter("ignore")
 
-import warnings
-warnings.simplefilter("ignore")
+#np.random.seed(0)
+dicts = {"sna": "SiO2-snap",
+         "so3": "SiO2-so3"}
 
-des, folder = "sna", "SiO2-snap"
-mliap  = folder + "/30-30-checkpoint.pth"
-lmpiap = folder + "/NN_weights.txt"
-lmpdes = folder + "/DescriptorParam.txt"
-
-
-# ase pyxtal_ff calculator
-ff = PyXtal_FF(model={'system': ["Si", "O"]}, logo=False)
-ff.run(mode='predict', mliap=mliap)
-calc_pff = PyXtalFFCalculator(ff=ff)
-
-# ase lammps calculatoor
-lammps_name=''
-comm=None
-log_file='lammps.log'
-cmd_args = ['-echo', 'log', '-log', log_file,
-            '-screen', 'none', '-nocite']
-lmp = lammps(lammps_name, cmd_args, comm)
-
-parameters = ["mass 1 28.0855",
-              "mass 2 15.9999",
-              "pair_style hybrid/overlay &",
-              "mliap model nn " + lmpiap + " descriptor " + des + " " + lmpdes + " &",
-              "zbl 1.0 2.0",
-              "pair_coeff 1 1 zbl 8.0 8.0",
-              "pair_coeff 2 2 zbl 14.0 14.0",
-              "pair_coeff 1 2 zbl 8.0 14.0",
-              "pair_coeff * * mliap O Si",
-              ]
-
-calc_lmp = LAMMPSlib(lmp=lmp, lmpcmds=parameters)
-
-np.random.seed(0)
-# check for single configuration
-for i in range(100):
-    # initial silicon crystal
-    si = read('lt_quartz.cif')
-    # set the ordering
-    si.set_tags([2]*3+[1]*6) 
-    si.positions += 0.25*(np.random.random_sample([len(si),3])-0.5)
-    eng = []
-    force = []
-    stress = []
-    for calc in [calc_lmp, calc_pff]:
-        si.set_calculator(calc)
-        eng.append(si.get_potential_energy())
-        force.append(si.get_forces())
-        stress.append(si.get_stress())
-    #calc.print_energy()
-    e_diff = eng[0]-eng[1]
-    f_diff = np.linalg.norm((force[0] - force[1]).flatten())
-    s_diff = np.linalg.norm((stress[0] - stress[1]).flatten())/units.GPa
-
-    print("{:3d} {:8.3f} eV {:8.3f} GPa {:8.3f} {:8.3f} {:8.3f}".format(i, eng[0], -stress[0][0]/units.GPa, e_diff, f_diff, s_diff))
-    if abs(e_diff) > 1e-2 or f_diff > 1e-2 or s_diff > 1e-2:
-        print("eng: ", eng[0], eng[1])
-        print("Forces from LAMMPS and PyXtal_FF")
-        for f1, f2 in zip(force[0], force[1]):
-            print("{:8.4f} {:8.4f} {:8.4f} -> {:8.4f} {:8.4f} {:8.4f} -> {:8.4f} {:8.4f} {:8.4f}".format(*f1, *f2, *(f1-f2)))
-        print("\n Breakdown of Pyxtal_FF")
-        calc.print_all()
-
+for des in dicts.keys():
+    print("\n", des)
+    folder = dicts[des]
+    mliap  = folder + "/30-30-checkpoint.pth"
+    lmpiap = folder + "/NN_weights.txt"
+    lmpdes = folder + "/DescriptorParam.txt"
+    
+    # ase pyxtal_ff calculator
+    ff = PyXtal_FF(model={'system': ["Si", "O"]}, logo=False)
+    ff.run(mode='predict', mliap=mliap)
+    calc_pff = PyXtalFFCalculator(ff=ff)
+    
+    # ase lammps calculatoor
+    lammps_name=''
+    comm=None
+    log_file='lammps.log'
+    cmd_args = ['-echo', 'log', '-log', log_file,
+                '-screen', 'none', '-nocite']
+    lmp = lammps(lammps_name, cmd_args, comm)
+    
+    parameters = ["mass 1 28.0855",
+                  "mass 2 15.9999",
+                  "pair_style hybrid/overlay &",
+                  "mliap model nn " + lmpiap + " descriptor " + des + " " + lmpdes + " &",
+                  "zbl 1.0 2.0",
+                  "pair_coeff 1 1 zbl 8.0 8.0",
+                  "pair_coeff 2 2 zbl 14.0 14.0",
+                  "pair_coeff 1 2 zbl 8.0 14.0",
+                  "pair_coeff * * mliap O Si",
+                  ]
+    
+    calc_lmp = LAMMPSlib(lmp=lmp, lmpcmds=parameters)
+    
+    # check for single configuration
+    for i in range(20):
+        # initial silicon crystal
+        si = read('lt_quartz.cif')
+        # set the ordering
+        si.set_tags([2]*3+[1]*6) 
+        si.positions += 0.25*(np.random.random_sample([len(si),3])-0.5)
+        eng = []
+        force = []
+        stress = []
+        for calc in [calc_lmp, calc_pff]:
+            si.set_calculator(calc)
+            eng.append(si.get_potential_energy())
+            force.append(si.get_forces())
+            stress.append(si.get_stress())
+        #calc.print_energy()
+        e_diff = eng[0]-eng[1]
+        f_diff = np.linalg.norm((force[0] - force[1]).flatten())
+        s_diff = np.linalg.norm((stress[0] - stress[1]).flatten())/units.GPa
+    
+        print("{:3d} {:8.3f} eV {:8.3f} GPa {:8.3f} {:8.3f} {:8.3f}".format(i, eng[0], -stress[0][0]/units.GPa, e_diff, f_diff, s_diff))
+        if abs(e_diff) > 1e-2 or f_diff > 1e-2 or s_diff > 1e-2:
+            print("eng: ", eng[0], eng[1])
+            print("Forces from LAMMPS and PyXtal_FF")
+            for f1, f2 in zip(force[0], force[1]):
+                print("{:8.4f} {:8.4f} {:8.4f} -> {:8.4f} {:8.4f} {:8.4f} -> {:8.4f} {:8.4f} {:8.4f}".format(*f1, *f2, *(f1-f2)))
+            print("\n Breakdown of Pyxtal_FF")
+            calc.print_all()
+            break
